e6555aee8d9e4dccadbf2f7aef8220ad7ae73731
Artem <47177513+AHradyskyi@users.noreply.github.com>
2020-07-14 23:16:12 +0300

    Add files via upload

--- dictionar.py
+++ dictionar.py
@@ -9,11 +9,24 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
-    elif len(get_close_matches(w,data.keys())) > 0:
-        return "Are U sure? I think it should be \"%s\".    " %get_close_matches(w,data.keys())[0]
+    elif len(get_close_matches(w, data.keys())) > 0:
+         yon = input("Are U sure? I think it should be \"%s\". Enter Y if yes or N if not: " %get_close_matches(w,data.keys())[0])
+         if yon == "Y":
+             return data[get_close_matches(w,data.keys())[0]]
+         elif yon == "N":
+             return "The word does not exists. Try again later."
+         else:
+             return "I am sorry, what was that?"
+
     else:
         return "The word does not exists. Try again later."
 
 
 word = input("Enter a word: ")
-print(translate(word))
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print('*',item)
+else:
+    print(output)
